720e73ab7647aac860a417d7d80ff8a4cdcdba28
AshrafOhi <ashrafohi20@gmail.com>
2024-03-18 11:52:53 +0600

    homebanner-added

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -1,28 +1,25 @@
 import { Button } from "flowbite-react";
+import { Link } from "react-router-dom";
 
 export default function CallToAction() {
   return (
     <div className="flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center">
       <div className="flex-1 justify-center flex flex-col">
-        <h2 className="text-2xl">Want to learn more about JavaScript?</h2>
-        <p className="text-gray-500 my-2">
-          Checkout these resources with 100 JavaScript Projects
+        <h2 className="text-2xl">Want to learn more about blogs?</h2>
+        <p className="text-gray-400 my-2">
+          Checkout these resources with your idea
         </p>
         <Button
           gradientDuoTone="purpleToPink"
           className="rounded-tl-xl rounded-bl-none"
         >
-          <a
-            href="https://www.100jsprojects.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            100 JavaScript Projects
-          </a>
+          <Link to="/search" target="_blank" rel="noopener noreferrer">
+            see blogs
+          </Link>
         </Button>
       </div>
       <div className="p-7 flex-1">
-        <img src="https://bairesdev.mo.cloudinary.net/blog/2023/08/What-Is-JavaScript-Used-For.jpg" />
+        <img src="https://t4.ftcdn.net/jpg/06/37/34/29/360_F_637342919_2XygScvMbUcwOzkH8lMozgaobhpiRnZh.jpg" />
       </div>
     </div>
   );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -17,10 +17,12 @@ export default function Home() {
   return (
     <div>
       <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto ">
-        <h1 className="text-3xl font-bold lg:text-6xl">Welcome to my Blog</h1>
-        <p className="text-gray-500 text-xs sm:text-sm">
+        <h1 className="text-3xl font-bold lg:text-6xl">
+          Welcome to Daily Blog
+        </h1>
+        <p className="text-gray-500 text-sm sm:text-xl">
           Here you'll find a variety of articles and tutorials on topics such as
-          web development, software engineering, and programming languages.
+          daily life, technology, and business.
         </p>
         <Link
           to="/search"
